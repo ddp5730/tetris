04f98d7f5eacbd9445d55755f932cbe4ea5635d6
Dan Popp <ddp5730@rit.edu>
2020-09-01 11:09:12 -0700

    Set up Clion Project.

---
+++ src/Tetris_Program.cpp
@@ -0,0 +1,10 @@
+//
+// Created by poppfd on 9/1/20.
+//
+
+#include <iostream>
+#include "Tetris_Program.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello World!" << std::endl;
+}
---
+++ src/Tetris_Program.h
@@ -0,0 +1,14 @@
+//
+// Created by poppfd on 9/1/20.
+//
+
+#ifndef TETRIS_TETRIS_PROGRAM_H
+#define TETRIS_TETRIS_PROGRAM_H
+
+
+class Tetris_Program {
+
+};
+
+
+#endif //TETRIS_TETRIS_PROGRAM_H
