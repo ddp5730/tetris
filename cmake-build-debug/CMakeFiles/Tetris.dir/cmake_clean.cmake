ed3c14aed88abf2613ab0f96f1152c8511813410
Dan Popp <ddp5730@rit.edu>
2020-09-01 11:13:03 -0700

    Updated .gitignore

---
+++ cmake-build-debug/CMakeFiles/Tetris.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris.dir/src/Tetris_Program.cpp.o"
+  "Tetris"
+  "Tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Tetris_Program.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tetris_Program.dir/src/Tetris_Program.cpp.o"
+  "Tetris_Program"
+  "Tetris_Program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tetris_Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
